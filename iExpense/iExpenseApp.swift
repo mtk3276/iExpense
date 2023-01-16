7c47a8cb8c26643ccd1b5df11e92caf0cbc1f86f
Muhsina Kamal <muhsina_k@hotmail.co.uk>
2023-01-16 20:09:46 +0000

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Muhsina Kamal on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
